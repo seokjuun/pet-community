acd61657736418c1c5ef0015cea89ab17c810ab3
DoyeonLee-Luna <ldon77@naver.com>
2025-05-08 17:31:18 +0900

    chore : 초기 세팅

---
+++ src/main/java/com/potato/petpotatocommunity/PetPotatoCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.potato.petpotatocommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPotatoCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetPotatoCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/potato/petpotatocommunity/PetPotatoCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.petpotatocommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPotatoCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
