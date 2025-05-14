0e6c48da85f49ec6c7079395f687d5af6cd4247f
DoyeonLee-Luna <ldon77@naver.com>
2025-05-14 10:01:50 +0900

    feat : 인기인기글 경로 추가

--- src/main/java/com/potato/petpotatocommunity/config/SecurityConfig.java
+++ src/main/java/com/potato/petpotatocommunity/config/SecurityConfig.java
@@ -16,24 +16,25 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // csrf 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청 허용
+                        .requestMatchers("/auth/**").permitAll() // /auth/ 로 시작하는 요청 인증 없이 접근 허용
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // swagger 허용
+                        .requestMatchers("/manager/**").hasRole("MANAGER") // 관리자만 접근 가능
+                        .requestMatchers("/user/**").hasRole("USER") // 사용자만 접근 가능
+                        .requestMatchers("/",
+                                         "/index.html",
+                                         "/login.html",
+                                         "/register.html",
+                                         "/mypage.html",
+                                         "/assets/**",
+                                         "/boards/**",
+                                         "/api/posts/**",
+                                         "/posts/popular",
+                                         "/mypage/**").permitAll()
+                        .anyRequest().authenticated() // 나머지 요청은 로그인 필요
+
+
+//                        .anyRequest().permitAll() // 모든 요청 허용
                 )
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/auth/**").permitAll() // /auth/ 로 시작하는 요청 인증 없이 접근 허용
-//                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // swagger 허용
-//                        .requestMatchers("/manager/**").hasRole("MANAGER") // 관리자만 접근 가능
-//                        .requestMatchers("/user/**").hasRole("USER") // 사용자만 접근 가능
-//                        .requestMatchers("/",
-//                                         "/index.html",
-//                                         "/login.html",
-//                                         "/register.html",
-//                                         "/mypage.html",
-//                                         "/assets/**",
-//                                         "/mypage/**").permitAll()
-//                        .anyRequest().authenticated() // 나머지 요청은 로그인 필요
-//
-//
-//                )
                 .formLogin(Customizer.withDefaults()) // 기본 제공 Spring Security 로그인 폼 활성화
                 .httpBasic(Customizer.withDefaults()) // Http Basic 인증 활성화
                 .logout(logout -> logout.disable()); // 필요 없다면 disable 가능
@@ -47,19 +48,3 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
-
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .anyRequest().permitAll() // 모든 요청 허용
-//                );
-//        return http.build();
-//    }
-//}
