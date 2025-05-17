68e5430e3b2d6e3cea88fcde828138c9f9de90e2
DoyeonLee-Luna <ldon77@naver.com>
2025-05-17 12:25:19 +0900

    feat : 로그인 안 한 경우 로그인 페이지 이동 링크 추가

--- src/main/resources/static/assets/js/auth.js
+++ src/main/resources/static/assets/js/auth.js
@@ -8,18 +8,21 @@ async function checkAuthStatus() {
             // 로그인 상태인 경우
             authButtons.innerHTML = `
                 <span style="margin-right: 10px;">${user.nickname || user.username}님</span>
-                <a href="mypage.html" class="btn btn-outline">마이페이지</a>
+                <a href="../mypage.html" class="btn btn-outline">마이페이지</a>
                 <button onclick="logout()" class="btn btn-primary">로그아웃</button>
             `;
         }
+
+        else {
+            // 로그인 상태가 아닌 경우 홈으로 리디렉션
+            window.location.href = '/login.html';
+        }
     } catch (error) {
         console.error('로그인 상태 확인 오류:', error);
+        window.location.href = '/login.html';
     }
 }
 
-/**
- * 로그아웃 함수
- */
 async function logout() {
     try {
         const response = await fetch('/auth/logout', {
